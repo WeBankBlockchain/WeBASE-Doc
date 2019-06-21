f3458eac1b12e574f63b74deaf25ebf60825ef53
grayson <zhanglong04@baidu.com>
2019-06-21 15:36:14 +0800

    .

--- conf.py
+++ conf.py
@@ -262,13 +262,24 @@ latex_elements = {
 'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-'pointsize': '10pt',
+'pointsize': '10pt','classoptions':',oneside','babel':'',
+'inputenc':'',
+'utf8extra':'',
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
 'preamble': r'''
 \hypersetup{unicode=true}
 \usepackage{CJKutf8}
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
 \DeclareUnicodeCharacter{00A0}{\nobreakspace}
 \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
 \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
