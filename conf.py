e837ab88313e3f9ac80769e7e4b2dea29bc1fb30
codingcattwo <847701726@qq.com>
2023-07-13 16:06:07 +0800

    add readthedoc yaml

--- conf.py
+++ conf.py
@@ -100,7 +100,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'WeBASE'
-copyright = u'© 2020. All rights reserved.'
+copyright = u'© 2023. All rights reserved.'
 author = u'WeBankBlockchain'
 
 # The version info for the project you're documenting, acts as replacement for
